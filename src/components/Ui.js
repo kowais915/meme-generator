789ae88813a236cd6dc3d09795a4b9c91feb16c7
Owais Khan <45107209+kowais915@users.noreply.github.com>
2023-03-15 16:23:43 +0500

    removed unsed functions and hooks

--- src/components/Ui.js
+++ src/components/Ui.js
@@ -1,4 +1,4 @@
-import {useState, useEffect, useCallback} from 'react';
+import {useState, useEffect} from 'react';
 
 const Ui = () => {
 
@@ -37,11 +37,11 @@ const getAMeme = ()=>{
    
 }
 
-const handleChange = (e) =>{
+// const handleChange = (e) =>{
 
-    e.preventDefault()
-    setUpperText(e.target.value)
-}
+//     e.preventDefault()
+//     setUpperText(e.target.value)
+// }
 
 
 
